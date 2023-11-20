14badd3efd123dd15b972df45f9a80d6712ad69a
Khalil Roland <khalilroland@Khalils-MacBook-Pro.local>
2023-11-20 02:30:06 -0500

    Finished incorperting lazyrow into friend list screen

--- app/src/main/java/com/example/plannerproject/FriendViewModel.kt
+++ app/src/main/java/com/example/plannerproject/FriendViewModel.kt
@@ -1,27 +1,51 @@
 package com.example.plannerproject
-import androidx.lifecycle.ViewModel
+
+import android.app.Application
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.plannerproject.User
-import com.example.plannerproject.UserRepository
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.plannerproject.*
+import kotlinx.coroutines.launch
 
-class FriendViewModel(itemsRepository: UserRepository) : ViewModel() {
+class FriendViewModel(app: Application) : AndroidViewModel(app) {
+    private val _users: MutableState<List<User>> = mutableStateOf(listOf())
+    val users: State<List<User>> = _users
+
+    private val _waiting: MutableState<Boolean>
+    val waiting: State<Boolean>
+
+    val testFriend = User(1,"TestFriend1",20)
 
 
-    val friendUiState: StateFlow<FriendUiState> =
-        itemsRepository.getAllUsersStream().map { FriendUiState(it) }
-            .stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
-                initialValue = FriendUiState()
-            )
+    private val _repository: UserRepository = UserOfflineRepository(getApplication())
 
-    companion object {
-        private const val TIMEOUT_MILLIS = 5_000L
+    init {
+        viewModelScope.launch {
+            _repository.insertUser(testFriend)
+            _users.value = _repository.getUsers()
+        }
+
+
+        _waiting = mutableStateOf(false)
+        waiting = _waiting
     }
-}
 
-data class FriendUiState(val itemList: List<User> = listOf())
+
+    fun addUser(user: User) {
+        viewModelScope.launch {
+            _waiting.value = true
+            _repository.insertUser(user)
+            _users.value = _repository.getUsers()
+            _waiting.value = false
+        }
+    }
+
+    suspend fun deleteUser(user: User) {
+        _repository.deleteUser(user)
+        _users.value = _repository.getUsers()
+    }
+
+}
